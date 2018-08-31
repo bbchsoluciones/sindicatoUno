31925613b8f7211ea440a089b27433db07fee30c
Cristobal <cristobalherreramkts@live.cl>
2018-08-31 15:08:05 -0300

    sidemenu changes

--- view/intranet/admin/include/sideMenu.php
+++ view/intranet/admin/include/sideMenu.php
@@ -1,85 +1,66 @@
-
-      <!-- Sidebar -->
-      <ul class="sidebar navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">
-                <i class="fas fa-fw fa-tachometer-alt pr-2"></i>
-                <span>Resumen</span>
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-users pr-2"></i>
-                <span>Usuario</span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                <h6 class="dropdown-header">Trabajador(a):</h6>
-                <a class="dropdown-item" href="userNew.php"><i class="fa fa-user-plus text-info pr-3"></i>Registrar</a>
-                <a class="dropdown-item" href="userManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
-                <div class="dropdown-divider"></div>
-                <h6 class="dropdown-header">Hijo(a):</h6>
-                <a class="dropdown-item" href="sonNew.php"><i class="fa fa-user-plus text-info pr-3"></i>Registrar</a>
-                <a class="dropdown-item" href="sonManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="fa fa-money-bill-alt pr-2"></i>
-                  <span>Movimiento</span>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                  <h6 class="dropdown-header">Ingreso y Egreso:</h6>
-                  <a class="dropdown-item" href="moveNew.php"><i class="fas fa-money-bill-wave text-info pr-3"></i>Registrar</a>
-                  <a class="dropdown-item" href="moveManage.php"><i class="fas fa-hand-holding-usd text-info pr-3"></i>Administrar</a>
-                  <div class="dropdown-divider"></div>
-                  <h6 class="dropdown-header">Gráficos:</h6>
-                  <a class="dropdown-item" href="barChartEntry.php"><i class="fas fa-chart-line text-info pr-3"></i>Ingresos</a>
-                  <a class="dropdown-item" href="barChartExit.php"><i class="fas fa-chart-bar text-info pr-3"></i>Egresos</a>
-                </div>
-              </li>
-            <!-- <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-money-bill-alt pr-2"></i>
-                <span>Movimiento</span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                <a class="dropdown-item" href="moveNew.php">Registrar</a>
-                <a class="dropdown-item" href="moveManage.php">Administrar</a>
-                <a class="dropdown-item" href="barChartEntry.php">Gráfico Ingreso</a>
-                <a class="dropdown-item" href="barChartExit.php">Gráfico Egreso</a>
-              </div>
-            </li> -->
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-newspaper pr-2"></i>
-                <span>Noticias</span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                <a class="dropdown-item" href="newNews.php"><i class="fa fa-plus text-info pr-3"></i>Crear noticia</a>
-                <a class="dropdown-item" href="newsManage.php"><i class="fa fa-address-card text-info pr-3"></i>Editar noticia</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-images pr-2"></i>
-                <span>Galería</span>
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-list-alt pr-2"></i>
-                <span>Reportes</span>
-              </a>
-            </li>
-            <!--
-            <li class="nav-item">
-              <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Charts</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Tables</span></a>
-            </li>-->
-          </ul>
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+  <li class="nav-item active">
+    <a class="nav-link" href="index.php">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span class="pl-2">Resumen</span>
+    </a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+      aria-expanded="false">
+      <i class="fa fa-users pr-2"></i>
+      <span>Usuario</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <h6 class="dropdown-header">Trabajador(a):</h6>
+      <a class="dropdown-item" href="userNew.php"><i class="fa fa-user-plus text-info pr-3"></i>Registrar</a>
+      <a class="dropdown-item" href="userManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
+      <div class="dropdown-divider"></div>
+      <h6 class="dropdown-header">Hijo(a):</h6>
+      <a class="dropdown-item" href="sonNew.php"><i class="fa fa-user-plus text-info pr-3"></i>Registrar</a>
+      <a class="dropdown-item" href="sonManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+      aria-expanded="false">
+      <i class="fa fa-money-bill-alt pr-2"></i>
+      <span>Movimiento</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <h6 class="dropdown-header">Ingreso y Egreso:</h6>
+      <a class="dropdown-item" href="moveNew.php"><i class="fas fa-money-bill-wave text-info pr-3"></i>Registrar</a>
+      <a class="dropdown-item" href="moveManage.php"><i class="fas fa-hand-holding-usd text-info pr-3"></i>Administrar</a>
+      <div class="dropdown-divider"></div>
+      <h6 class="dropdown-header">Gráficos:</h6>
+      <a class="dropdown-item" href="barChartEntry.php"><i class="fas fa-chart-line text-info pr-3"></i>Ingresos</a>
+      <a class="dropdown-item" href="barChartExit.php"><i class="fas fa-chart-bar text-info pr-3"></i>Egresos</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+      aria-expanded="false">
+      <i class="fa fa-list-alt pr-2"></i>
+      <span>Reportes</span>
+    </a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+      aria-expanded="false">
+      <i class="fa fa-file-alt pr-2"></i>
+      <span>Pagina Principal</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <h6 class="dropdown-header">Inicio</h6>
+      <a class="dropdown-item" href="#"><i class="fa fa-align-justify text-info pr-3"></i>Editar</a>
+      <div class="dropdown-divider"></div>
+      <h6 class="dropdown-header">Galería</h6>
+      <a class="dropdown-item" href="#"><i class="fa fa-images text-info pr-3"></i>Administrar</a>
+      <h6 class="dropdown-header">Noticias</h6>
+      <a class="dropdown-item" href="newNews.php"><i class="fa fa-user-plus text-info pr-3"></i>Crear</a>
+      <a class="dropdown-item" href="newsManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
+      <div class="dropdown-divider"></div>
+    </div>
+  </li>
+</ul>
