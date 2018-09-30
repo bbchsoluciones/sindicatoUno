4311c42e36f94fa1585ecfa85f638c7f9765cb93
Cristobal <cristobalherreramkts@live.cl>
2018-09-30 18:08:34 -0300

    limpieza imagenes

--- view/intranet/admin/include/sideMenu.php
+++ view/intranet/admin/include/sideMenu.php
@@ -68,8 +68,8 @@
       </a>
       <div class="dropdown-menu" aria-labelledby="pagesDropdown">
         <a class="dropdown-item" href="newNews.php"><i class="fa fa-user-plus text-info pr-3"></i>Crear</a>
-        <a class="dropdown-item" href="newsManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
         <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="newsManage.php"><i class="fa fa-address-card text-info pr-3"></i>Administrar</a>
       </div>
     </li>
     <li class="nav-item dropdown">
@@ -80,8 +80,8 @@
       </a>
       <div class="dropdown-menu" aria-labelledby="pagesDropdown">
         <a class="dropdown-item" href="newGallery.php"><i class="fas fa-file-upload text-info pr-3"></i>Subir</a>
-        <a class="dropdown-item" href="galleryManage.php"><i class="far fa-images text-info pr-3"></i>Administrar</a>
         <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="galleryManage.php"><i class="far fa-images text-info pr-3"></i>Administrar</a>
       </div>
     </li>
     <li class="nav-item">
